b9be1a832a0092fbf21b73c21f540de269eb52d1
Harmanpawar15 <harmanpawar@HarmanpreetsAir.ht.home>
2023-03-06 18:43:19 -0500

    Created a new Project, Intial screen: Added a custom buttom

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TouchableOpacity } from 'react-native';
+
+//for customizing start button:
+const StartButton = ({onPress, title}) => (
+  <TouchableOpacity onPress={onPress} style={styles.btnStart}>
+    <Text style={styles.startBtnText}>{title}</Text>
+  </TouchableOpacity>
+);
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+   <StartButton title="START"  size = "sm"  ></StartButton>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  //for main view
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor:'pink' ,
     justifyContent: 'center',
+   // padding:16 ,
+    alignContent:'center' ,
+    flexDirection:'column' ,
   },
+  //for button
+  btnStart:{
+   elevation: 19,    //means shadow for btn 
+   paddingVertical:10 ,
+   paddingHorizontal:12 ,
+   backgroundColor:'orange' ,
+   borderRadius:68,
+   alignItems:'center',
+   justifyContent: 'center',
+  } ,
+  //text in btn
+  startBtnText:{
+    fontSize:25 ,
+    color:'white' ,
+    fontWeight:'bold' ,
+    alignSelf:'center'
+  } ,
+  
+ 
 });
